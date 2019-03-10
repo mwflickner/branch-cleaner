c2fb1cef3903921a5788240c6a17499b9609bfae
Matthew <mwflickner@gmail.com>
2019-03-10 14:38:42 -0500

    First simple try

--- branch_cleaner.rb
+++ branch_cleaner.rb
@@ -1,10 +1,16 @@
-def get_branches()
-  branches = `git branch`
-  puts branches
-end
+require 'set'
 
 def clean_branches()
-  branches_to_keep = ['master']
+  branches = `git branch`.split("\n")
+  branches_to_keep = Set.new ["master"]
+  for branch in branches do
+    branch = branch.gsub(/^\s+/, '')
+    if branch.start_with?('*') || branches_to_keep.include?(branch)
+      puts "Skipping branch " + branch.gsub(/^\*?\s*/, '')
+    else
+      system("git branch -d " + branch)
+    end
+  end
 end
 
-get_branches()
+clean_branches()
